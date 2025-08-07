025e67f7e4eb9ee0be0368399d0f8bb35a953cde
dongwookim <132253329+devdongwoo@users.noreply.github.com>
2025-08-07 10:13:22 +0900

    공통컴포넌트 arrow function 표기 + prisma dml문으로 컬럼변경 + schema 적용 (#44)

--- app/_components/Button/Button.tsx
+++ app/_components/Button/Button.tsx
@@ -4,32 +4,32 @@ import { Button as AntdButton } from "antd";
 //차후 tailwind css 사용할때 상수화
 
 interface IButton {
-  children: React.ReactNode;
-  type: "default" | "primary" | "link" | "text" | "dashed";
-  color:
-    | "default"
-    | "primary"
-    | "danger"
-    | "blue"
-    | "purple"
-    | "cyan"
-    | "green"
-    | "magenta"
-    | "pink"
-    | "red"
-    | "orange"
-    | "yellow"
-    | "volcano"
-    | "geekblue"
-    | "lime"
-    | "gold";
-  onClick: () => void;
+    children: React.ReactNode;
+    type: "default" | "primary" | "link" | "text" | "dashed";
+    color:
+        | "default"
+        | "primary"
+        | "danger"
+        | "blue"
+        | "purple"
+        | "cyan"
+        | "green"
+        | "magenta"
+        | "pink"
+        | "red"
+        | "orange"
+        | "yellow"
+        | "volcano"
+        | "geekblue"
+        | "lime"
+        | "gold";
+    onClick: () => void;
 }
 
-export default function Button({ children, type, color, onClick }: IButton) {
-  return (
-    <AntdButton type={type} color={color} onClick={onClick}>
-      {children}
-    </AntdButton>
-  );
+export const Button = ({ children, type, color, onClick }: IButton) => {
+    return (
+        <AntdButton type={type} color={color} onClick={onClick}>
+            {children}
+        </AntdButton>
+    );
 }

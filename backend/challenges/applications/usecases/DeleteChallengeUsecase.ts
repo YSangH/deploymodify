25f9b8ed1759983860a16bf639070af86a2806b5
HarenKei <seungmin1266@gmail.com>
2025-08-24 23:25:22 +0900

    fix : 챌린지 삭제 유스케이스에서 id 검증 추가

--- backend/challenges/applications/usecases/DeleteChallengeUsecase.ts
+++ backend/challenges/applications/usecases/DeleteChallengeUsecase.ts
@@ -4,10 +4,14 @@ import { Challenge } from '@/backend/challenges/domains/entities/Challenge';
 // 챌린지 삭제 유스케이스
 export class DeleteChallengeUsecase {
   // 리포지토리 주입
-  constructor(private readonly challengeRepo: IChallengeRepository) {}
+  constructor(private readonly challengeRepo: IChallengeRepository) { }
 
   // 챌린지 삭제 실행
   async execute(challenge: Challenge): Promise<void> {
+    if (!challenge.id) {
+      throw new Error('챌린지 ID가 없습니다.');
+    }
+
     const deletedChallenge = await this.challengeRepo.delete(challenge.id);
     if (!deletedChallenge) {
       throw new Error('챌린지 삭제 실패');
